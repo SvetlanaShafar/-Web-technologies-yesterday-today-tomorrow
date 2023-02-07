71fdf64c3633e84ef931d7a5258c4dfb31b7f904
Svetlana <sveta.yakovenko97@mail.ru>
2023-02-07 14:10:29 +0300

    Add DZ4

---
+++ DZ4/index.php
@@ -0,0 +1,179 @@
+<?php
+$mWork = [
+    [
+        "post" => "Waiter",
+        "start" => "September 2016",
+        "finish" => "Jun 2017",
+        "responsibilities" => "quidem sed consequatur voluptatem dolorem quaerat et, saepe blanditiis, magnam non quod maxime cupiditate perferendis excepturi modi. Magni architecto facilis id excepturi.
+        Quis eius libero accusamus maiores architecto error fugit placeat atque quam consequatur eum optio reprehenderit natus ipsam, dolore quae voluptatum quasi? Incidunt, explicabo. Sed voluptate sint odio placeat mollitia totam.
+        Aperiam quibusdam magni ipsum, ratione fugiat quidem officiis cupiditate ad dicta recusandae nulla voluptatum ipsa porro velit eaque reiciendis eius. Necessitatibus alias doloremque nihil quibusdam iusto doloribus impedit? Nobis, itaque.",
+        
+    ],
+    [
+        "post" => "Assistant engineer",
+        "start" => "September 2017",
+        "finish" => "February 2020",
+        "responsibilities" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit totam aliquid quod a nobis cupiditate vero hic assumenda, libero tempora dignissimos optio temporibus! Dicta ut est repudiandae beatae, at ratione!
+        Corrupti et quibusdam deleniti late ad dicta recusandae nulla voluptatum ipsa porro velit eaque reiciendis eius. Necessitatibus alias doloremque nihil quibusdam iusto doloribus impedit? Nobis, itaque.",
+        
+    ],
+    
+    [
+        "post" => "Design engineer HVAK",
+        "start" => "February 2020",
+        "finish" => "at present",
+        "responsibilities" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit totam aliquid quod a nobis cupiditate vero hic assumenda, libero tempora dignissimos optio temporibus! Dicta ut est repudiandae beatae, at ratione!
+        Corrupti et quibusdam deleniti libero blanditiis assumenda commodi dolorem ipsam eligendi magnam similique, culpa provident corporis iusto quidem suscipit eveniet itaque optio dolore quia. Est amet rem fugit consectetur qui.
+        Rem, rerum id ad voluptatum similique qui optio quidem sed consequatur voluptatem dolorem quaerat et, saepe blanditiis, magnam non quod maxime cupiditate perferendis excepturi modi. Magni architecto facilis id excepturi.",
+        
+    ],
+    
+]
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Мое резюме</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    html,body,h1,h2,h3,h4,h5,h6 {font-family: 'Jost', sans-serif;}
+    </style>
+  </head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2>Имя</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Профессия</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Город, страна</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>E-mail</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>Телефон</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Навыки</b></p>
+          <p>Adobe Photoshop</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Photography</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Illustrator</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">75%</div>
+          </div>
+          <p>Media</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b></p>
+          <p>Английский</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>Испанский</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
+          </div>
+          <p>Немецкий</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+        
+        <?php for ($i = 0; $i <count($mWork); $i++): ?>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b><?php echo $mWork[$i]['post']; ?> </b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $mWork[$i]['start']; ?> - 
+          <?php if ($i == 2): ?>
+          <span class="w3-tag w3-teal w3-round">
+            <?php echo $mWork[$i]['finish']; ?>
+        </span>
+        <?php else: ?>
+            <?php echo $mWork[$i]['finish']; ?>
+<?php endif; ?>
+    </h6>
+          
+          <p><?php echo $mWork[$i]['responsibilities']; ?></p>
+          <hr>
+        </div>
+        <?php endfor; ?>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>gb.ru</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+          <p>Web Development! All I need to know in one place</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>London Business School</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+          <p>Master Degree</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>School of Coding</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+          <p>Bachelor Degree</p><br>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+ <!-- End footer -->
+ </footer>
+
+</body>
+</html>
